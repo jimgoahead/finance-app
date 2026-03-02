976eccab88af946c3435fff83895f3ab579ea06e
jimgoahead <jimgoahead@gmail.com>
2026-03-02 15:34:37 +0700

    Update App.py

--- App.py
+++ App.py
@@ -45,7 +45,7 @@ st.markdown("""
         border-radius: 8px;
         height: 50px;
         font-weight: bold;
-        font-size: 18px;
+        font-size: 15px;
     }
     </style>
 """, unsafe_allow_html=True)
@@ -252,5 +252,6 @@ if not df.empty:
             st.dataframe(filtered_df[cols_to_show].sort_values(by='วันที่', ascending=False), use_container_width=True)
 else:
     st.info("ยังไม่มีข้อมูลเลยค่ะ เจ้านายลองบันทึกรายการแรกดูนะคะ!")
+
 
 
