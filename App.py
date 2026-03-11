ea45f250adb34e1e6c44183a23307bd13b15a74f
jimgoahead <jimgoahead@gmail.com>
2026-03-11 10:41:05 +0700

    Update App.py

--- App.py
+++ App.py
@@ -335,7 +335,6 @@ st.markdown("---")
 # ==========================================
 # ส่วนที่ 3: Dashboard & Cashflow Tabs
 # ==========================================
-st.markdown("### 📊 Super Dashboard ")
 st.markdown("### <span style='color: #00BFFF;'>📊 Super Dashboard Input</span>", unsafe_allow_html=True)
 
 # 💡 ลอจิกใหม่: แสดงสวิตช์เฉพาะตอนอยู่โหมดนักท่องเที่ยวเท่านั้น
@@ -454,6 +453,7 @@ if show_dashboard:
                 else: st.warning("⚠️ กรุณาเลือกเดือนที่ต้องการดู Cashflow ค่ะ")
     else:
         st.info("ยังไม่มีข้อมูลเลยค่ะ เจ้านายลองบันทึกรายการแรกดูนะคะ!")
+
 
 
 
